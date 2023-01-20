60be8d61c67e0dd017fc12552df4fd980428e786
hibo2261 <hborse1@gmail.com>
2023-01-20 09:06:36 +0530

    [Himanshu] : Added event listener for range slider

---
+++ range slider.js
@@ -0,0 +1,8 @@
+
+
+
+let salarySelector = document.querySelector('#salary');
+salarySelector.addEventListener("change", function () {
+    let rangeValue = document.getElementById('salary');
+    document.getElementById('salaryOutput').value = rangeValue.value;
+});
